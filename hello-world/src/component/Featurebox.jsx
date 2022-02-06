a0beca7d8feab4bc79dd1cf203b3183be9682b0c
Jakub Frydrych <jakub.frydrych@op.pl>
2022-02-06 17:28:16 +0100

    stylizacja

---
+++ hello-world/src/component/Featurebox.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Featurebox(props) {
+    return (
+        <div className='a-box'>
+           
+            
+        </div>
+    )
+}
+
+export default Featurebox
